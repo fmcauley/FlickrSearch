9e2981b1fc7c1b88ccc020e4a8e8a59931f3ebc2
Frank McAuley <fmcauley@resource.com>
2013-04-10 11:52:33 -0400

    pulling in the flicker images.

---
+++ Flickr Search/RFMFlickrPhotoCell.h
@@ -0,0 +1,13 @@
+//
+//  RFMFlickrPhotoCell.h
+//  Flickr Search
+//
+//  Created by Frank McAuley on 4/10/13.
+//  Copyright (c) 2013 Frank McAuley. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RFMFlickrPhotoCell : UICollectionViewCell
+
+@end
